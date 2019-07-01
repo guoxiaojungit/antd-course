e30a5cbc2da208770aa10e66271a084c7fd5daf8
guoxiaojungit <1535060321@qq.com>
2019-07-01 21:58:11 +0800

    dva

--- config/config.js
+++ config/config.js
@@ -2,11 +2,32 @@ export default {
     singular:true,
     plugins:[
         ['umi-plugin-react',{
-
+            antd:true,
+            dva:true
         }],
     ],
     routes: [{
         path:'/',
-        component:'./HelloWorld'
+        component:'../layout',
+        routes: [
+            {
+                path:'/',component: './HelloWorld'
+            },
+            {
+                path:'/helloworld',component:'./HelloWorld'
+            },
+            {
+                path: '/puzzlecards', component: './puzzlecards'
+            },
+            {
+                path:'/dashboard',
+                routes:[
+                    {path:'/dashboard/analysis',component:'./Dashboard/Analysis'},
+                    {path:'/dashboard/monitor',component:'./Dashboard/Monitor'},
+                    {path:'/dashboard/workplace',component:'./Dashboard/Workplace'}
+                ]
+            }
+        ]
+
     }]
 };
